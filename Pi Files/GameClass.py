6678411fb4d06fd80dee884e7a9d77a0a2721b1c
BranDenn <76827979+BranDenn@users.noreply.github.com>
2023-04-28 21:25:14 -0700

    various fixes, better font, hopefully better game screen

---
+++ Pi Files/GameClass.py
@@ -0,0 +1,27 @@
+class Game():
+    def __init__(self, leds = None, display = None, image = None, draw = None, font = None) -> None:
+        self.can_play = True
+        self.leds = leds
+        self.disp = display
+        self.img = image
+        self.draw = draw
+        self.font = font
+        self.ROWS = 11
+        self.COLUMNS = 11
+        self.OFF = [0, 0, 0]
+        self.RED = [255, 0, 0]
+        self.BLUE = [0, 0, 255]
+        self.WHITE = [255, 255, 255]
+
+    def __del__(self) -> None:
+        print(self.__class__.__name__, "has been destructed!")
+
+    def game_display(self) -> None:
+        self.draw.rectangle((0, 0, self.disp.width, self.disp.height), fill = (0, 0, 0))
+        self.draw.rectangle((0, 0, self.disp.width, 30), fill = (255, 0, 0))
+        self.draw.rectangle((0, self.disp.height - 30, self.disp.width, self.disp.height), fill = (255, 0, 0))
+        self.draw.text((5, 7.5), self.__class__.__name__, font = self.font, fill = (255, 255, 255))
+        
+        self.draw.text((5, 135), ("Hold Button for 2s"), font = self.font, fill = (255, 255, 255))
+        self.draw.text((5, 145), ("to return to menu."), font = self.font, fill = (255, 255, 255))
+        self.disp.display(self.img)
